c788d8a6a735199ff401f888d81d2e65dc965e55
Nixon <nixonkoech200@gmail.com>
2024-11-06 12:48:08 +0300

    Add routes for page navigation
    Add route paths to HomePage, SignUp, Login, and About page components and wrap them with the routes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import SignupForm from './pages/SignUpForm';
+import LoginForm from './pages/LoginForm';
+import AboutMe from './pages/AboutMe';
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+    <Layout>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/signup" element={<SignupForm />} />  
+      <Route path="/about" element={<AboutMe />} />
+    </Routes>
+    </Layout>
+  </Router>
+
+  );
 }
 
 export default App
