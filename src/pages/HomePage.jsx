524c2bd80d554d213dd1d205503b71486746f80c
Nixon <nixonkoech200@gmail.com>
2024-11-09 22:21:10 +0300

    Style the HeroSection component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,6 +8,8 @@ const HomePage = () => {
         <main>
         <div className="container mx-auto p-4 pt-20 relative overflow-x-hidden">
             <Navbar />
+        </div>
+        <div className="container mx-0 p-0 left-0 fixed top-0">
             <HeroSection />
         </div>
         </main>
