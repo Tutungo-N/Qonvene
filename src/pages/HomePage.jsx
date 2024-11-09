53d31426212a3348cace4b8ca78aefaf0eac3d3d
Nixon <nixonkoech200@gmail.com>
2024-11-09 19:46:50 +0300

    Render HeroSection component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "../components/NavBar";
+import HeroSection from "../components/HeroSection";
 
 const HomePage = () => {
 
     return(
-        <div className="container mx-auto p-4 pt-20">
+        <main>
+        <div className="container mx-auto p-4 pt-20 relative overflow-x-hidden">
             <Navbar />
-            
+            <HeroSection />
         </div>
+        </main>
     );
 };
 
