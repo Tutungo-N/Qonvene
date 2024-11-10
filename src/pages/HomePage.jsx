83377c727aa173dd52233f3c503b93f6bf5d0807
Nixon <nixonkoech200@gmail.com>
2024-11-10 15:37:45 +0300

    Modify styling to avoid overlapping of components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/NavBar";
 import HeroSection from "../components/HeroSection";
 import EventCarousel from "../components/EventCarousel";
+import Testimonials from "../components/Testimonials";
 
 const HomePage = () => {
 
@@ -13,9 +14,13 @@ const HomePage = () => {
         <div className="container mx-0 p-0 left-0 w-full h-screen">
             <HeroSection />
         </div>
-        <div className="container mx-auto mt-[-100vh] px-0 py-4 relative top-[100vh]">
+        <div className="w-full px-0 py-0">
             <EventCarousel />
         </div>
+        <div className="w-full px-0 py-0">
+            <Testimonials />
+        </div>
+
         </main>
     );
 };
