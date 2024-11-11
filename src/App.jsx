32b66d34ef41ba13b7b82db9aa296ebacc94ffd1
Nixon <nixonkoech200@gmail.com>
2024-11-11 14:08:40 +0300

    Use AnimatePresence from framer-motion for smooth navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import Signup from './pages/Signup';
@@ -8,19 +9,29 @@ import Login from './pages/Login';
 import AboutMe from './pages/AboutMe';
 
 function App() {
+  const location = useLocation();
+
   return (
-    <Router>
+    //<Router>
     <Layout>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />  
-      <Route path="/about" element={<AboutMe />} />
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />  
+        <Route path="/about" element={<AboutMe />} />
+      </Routes>
+    </AnimatePresence>
     </Layout>
-  </Router>
+  //</Router>
 
   );
 }
 
-export default App;
+const AnimatedApp = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AnimatedApp;
