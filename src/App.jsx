5e1d7a301445caf63a3abc76780146190c31d45d
Nixon <nixonkoech200@gmail.com>
2024-11-11 16:39:12 +0300

    Render Dashboard in App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import HomePage from './pages/HomePage';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import AboutMe from './pages/AboutMe';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   const location = useLocation();
@@ -17,6 +18,7 @@ function App() {
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />  
         <Route path="/about" element={<AboutMe />} />
