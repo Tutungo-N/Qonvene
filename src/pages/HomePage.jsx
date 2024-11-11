3cfa35bbb38782c0d074f2c06ecced7875d0ba0a
Nixon <nixonkoech200@gmail.com>
2024-11-11 13:21:44 +0300

    Add useRef hook for navigating to EventCarousel section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import Navbar from "../components/NavBar";
 import HeroSection from "../components/HeroSection";
 import EventCarousel from "../components/EventCarousel";
 import Testimonials from "../components/Testimonials";
 
 const HomePage = () => {
+const eventCarouselRef = useRef(null);
+
+const scrollToEventCarousel = () => {
+    if (eventCarouselRef.current) {
+        console.log("Scrolling to EventCarousel");
+        eventCarouselRef.current.scrollIntoView({ behavior: "smooth" });
+    } else {
+        console.error("EventCarousel ref not found.");
+    }
+    
+};
 
     return(
         <main className="bg-teal-500 left-0 w-screen mx-0 p-0">
@@ -12,9 +23,9 @@ const HomePage = () => {
             <Navbar />
         </div>
         <div className="py-0 left-0 w-screen h-screen">
-            <HeroSection />
+            <HeroSection scrollToEventCarousel={scrollToEventCarousel} />
         </div>
-        <div className="w-full px-0 py-0">
+        <div ref={eventCarouselRef} className="w-full px-0 py-0">
             <EventCarousel />
         </div>
         <div className="w-full px-0 py-0">
